72f41ad0eda4cb092f124647a059005e2111a95a
Andrew Motko <andrew.motko@gmail.com>
2023-10-18 07:42:53 +0300

    gallery added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const rootGallery = document.querySelector(".gallery");
+const imageGallery = createGallery(galleryItems);
+
+function createGallery(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+    </li>
+    `;
+    })
+    .join("");
+}
+
+rootGallery.insertAdjacentHTML("afterbegin", imageGallery);
+
+// Cтворюємо хендлєр відкриття модального вікна по кліку на зображення
+
+rootGallery.addEventListener("click", handleClickOnImage);
+
+function handleClickOnImage(evt) {
+  evt.preventDefault();
+  const { target } = evt;
+  if (!target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${target.dataset.source}", width="800" height="600">`
+  );
+  instance.show();
+
+  // Закриття модального вікна по натиску на Escape
+
+  window.addEventListener("keydown", onEscape);
+  function onEscape(evt) {
+    const ESC_KEY_CODE = "Escape";
+    if (evt.code === ESC_KEY_CODE) {
+      instance.close();
+      window.removeEventListener("keydown", onEscape);
+    }
+  }
+}
