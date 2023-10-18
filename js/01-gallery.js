762bcb31510da2920078e6e1f21e48e7d9899c22
Andrew Motko <andrew.motko@gmail.com>
2023-10-18 19:19:43 +0300

    second task finished

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,14 +8,14 @@ function createGallery(arr) {
     .map(({ preview, original, description }) => {
       return `
     <li class="gallery__item">
-    <a class="gallery__link" href="${original}">
-    <img
-    class="gallery__image"
-    src="${preview}"
-    data-source="${original}"
-    alt="${description}"
-    />
-    </a>
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
     </li>
     `;
     })
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const rootGallery = document.querySelector(".gallery");
+const imageGallery = createGallery(galleryItems);
+
+function createGallery(arrOfObj) {
+  return arrOfObj
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+    `;
+    })
+    .join("");
+}
+
+rootGallery.insertAdjacentHTML("afterbegin", imageGallery);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  animationSpeed: 250,
+  overlay: true,
+  showCounter: false,
+  fadeSpeed: 200,
+  close: true,
+  showCounter: true,
+});
